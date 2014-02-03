01059e6abf7387ba7f67c5288f5ae248578ceaf2
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-03 21:16:19 +0100

    added admin role

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
         lastName: String,
         username: String,
         salt: String,
-        hashed_pwd: String
+        hashed_pwd: String,
+        roles: [String]
     });
 
     userSchema.methods = {
@@ -31,11 +32,11 @@ module.exports = function(config) {
            var salt, hash;
            salt = createSalt();
            hash = hashPwd(salt, 'foo');
-           User.create({firstName: 'foo', lastName: 'bar', username: 'foobar', salt: salt, hashed_pwd: hash});
+           User.create({firstName: 'foo', lastName: 'bar', username: 'foobar', salt: salt, hashed_pwd: hash, roles: ['admin']});
 
            salt = createSalt();
            hash = hashPwd(salt, 'john');
-           User.create({firstName: 'john', lastName: 'doe', username: 'pass', salt: salt, hashed_pwd: hash});
+           User.create({firstName: 'john', lastName: 'doe', username: 'pass', salt: salt, hashed_pwd: hash, roles: []});
        }
     });
 };
