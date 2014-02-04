1c28771fc905457b01684efd107fb47a444592e6
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-04 19:46:44 +0100

    unique username

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    encryption = require('../utils/encryption');
+    userModel = require('../models/User');
 
 module.exports = function(config) {
     mongoose.connect(config.db);
@@ -10,33 +10,5 @@ module.exports = function(config) {
         console.log('multivision db open');
     });
 
-    var userSchema = mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        username: String,
-        salt: String,
-        hashed_pwd: String,
-        roles: [String]
-    });
-
-    userSchema.methods = {
-        authenticate: function(passwordToMatch) {
-            return encryption.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-        }
-    };
-
-    var User = mongoose.model('User', userSchema);
-
-    User.find({}).exec(function(err, collection){
-       if ( collection.length === 0) {
-           var salt, hash;
-           salt = encryption.createSalt();
-           hash = encryption.hashPwd(salt, 'foo');
-           User.create({firstName: 'foo', lastName: 'bar', username: 'foobar', salt: salt, hashed_pwd: hash, roles: ['admin']});
-
-           salt = encryption.createSalt();
-           hash = encryption.hashPwd(salt, 'john');
-           User.create({firstName: 'john', lastName: 'doe', username: 'pass', salt: salt, hashed_pwd: hash, roles: []});
-       }
-    });
+    userModel.createDefaultUsers();
 };
---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose'),
+    encryption = require('../utils/encryption');
+
+var userSchema = mongoose.Schema({
+    firstName: { type: String, required: '{PATH} is required!' },
+    lastName: { type: String, required: '{PATH} is required!' },
+    username: {
+        type: String,
+        required: '{PATH} is required!',
+        unique: true
+    },
+    salt: { type: String, required: '{PATH} is required!' },
+    hashed_pwd: { type: String, required: '{PATH} is required!' },
+    roles: [String]
+});
+
+userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+        return encryption.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+};
+
+var User = mongoose.model('User', userSchema);
+
+function createDefaultUsers() {
+    User.find({}).exec(function(err, collection){
+        if ( collection.length === 0) {
+            var salt, hash;
+            salt = encryption.createSalt();
+            hash = encryption.hashPwd(salt, 'foo');
+            User.create({firstName: 'foo', lastName: 'bar', username: 'foobar', salt: salt, hashed_pwd: hash, roles: ['admin']});
+
+            salt = encryption.createSalt();
+            hash = encryption.hashPwd(salt, 'john');
+            User.create({firstName: 'john', lastName: 'doe', username: 'pass', salt: salt, hashed_pwd: hash, roles: []});
+        }
+    });
+}
+
+exports.createDefaultUsers = createDefaultUsers;
