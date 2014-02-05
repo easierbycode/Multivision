7de5fec50a117bcf4986aa8f9c645cb1f67a3520
Jaxwood <lorenzen.jacob@gmail.com>
2014-02-05 17:59:00 +0100

    added caching to courses list

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,8 +1,10 @@
-angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse){
-    $scope.courses = mvCourse.query();
+angular.module('app').controller('mvCourseListCtrl', function($scope, mvCachedCourses){
+    $scope.courses = mvCachedCourses.query();
+
     $scope.sortOptions = [
         {value:'title', text: 'Sort by title'},
         {value:'published',text:'Sort by Publish Date'}
     ];
+
     $scope.sortOrder = $scope.sortOptions[0].value;
 });
